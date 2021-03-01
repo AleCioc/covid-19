2dfec1f0c6a0c2d803fa2cd8cdd118472e124dd9
francescodeaglio <francesco.deaglio@gmail.com>
2021-03-01 18:42:32 +0100

    Reorganized directories. Added test folder

--- covid_19/data_manager/__init__.py
+++ covid_19/data_manager/__init__.py
@@ -3,13 +3,14 @@ import git
 from covid_19.data_manager.config.config import *
 
 
-rawdatapath = os.path.join(os.getcwd(), "covid_19", "data_manager", "data", "raw", "cases", "italy")
-
+rawdatapath = os.path.join(os.getcwd(), "covid_19", "data_manager", "data", "raw", "cases", "italy", "COVID-19")
 
 directory = os.listdir(rawdatapath)
 
+
+
 # controllare se esiste la directory e clonare solo se non c'è
-if not os.path.exists(rawdatapath):
+if not os.path.exists(rawdatapath) or len(directory) == 0:
     git.Repo.clone_from("https://github.com/pcm-dpc/COVID-19", rawdatapath)
 
 
