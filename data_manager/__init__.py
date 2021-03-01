b7cea076a9b4647e4818ddc240a6aa7529013546
francescodeaglio <francesco.deaglio@gmail.com>
2021-03-01 12:39:25 +0100

    Add git clone in data_manager

--- data_manager/__init__.py
+++ data_manager/__init__.py
@@ -2,6 +2,17 @@ import git
 
 from data_manager.config.config import *
 
+
+rawdatapath = os.path.join(os.getcwd(), "data_manager", "data", "raw", "cases", "italy")
+
+
+directory = os.listdir(rawdatapath)
+
+# controllare se esiste la directory e clonare solo se non c'è
+if not os.path.exists(rawdatapath):
+    git.Repo.clone_from("https://github.com/pcm-dpc/COVID-19", rawdatapath)
+
+
 g = git.cmd.Git(raw_cases_paths_dict["italy"])
 g.pull()
 
